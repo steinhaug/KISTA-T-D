5feab794a94431ff34f5d2832640f5d3eb523c90
Kim Steinhaug <kim@steinhaug.com>
2024-10-16 19:28:09 +0200

    Project updates

---
+++ environment.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// Set global variables
+date_default_timezone_set("Europe/Oslo");
+mb_internal_encoding('UTF-8');
+setlocale(LC_TIME, "nb_NO.utf8");
+
+define('HOME_PATH', dirname(__FILE__));
+require HOME_PATH . '/credentials.php';
+
